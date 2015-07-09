b7aae4a82d872f4f27a67ee380bfd6cd6d7568d5
Li-Wen Hsu <lwhsu@lwhsu.org>
2015-07-09 17:11:01 +0800

    remove comments

--- hello.c
+++ hello.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
 // this is a comment
-// this is another comment
-// this is still a comment
-// this is the 4th comment
-// this is the 5th comment
 
 int main(int argc, const char *argv[])
 {
